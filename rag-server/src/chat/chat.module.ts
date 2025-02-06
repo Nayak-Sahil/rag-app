e2f8c7ee798c35ad4110262d48c15e9e930f72c4
Sahil Nayak <62712927+Nayak-Sahil@users.noreply.github.com>
2025-02-06 11:04:37 +0530

    Add: env ConfigService Setup

--- rag-server/src/chat/chat.module.ts
+++ rag-server/src/chat/chat.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ChatService } from './chat.service';
 import { ChatController } from './chat.controller';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [ChatController],
-  providers: [ChatService],
+  providers: [ChatService, ConfigService],
 })
 export class ChatModule {}
--- rag-server/src/chat/chat.service.ts
+++ rag-server/src/chat/chat.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class ChatService {
+  constructor(private readonly configService: ConfigService) {}
+
   show(message: string) {
+    console.log(this.configService.get<String>('API_KEY'));
     return message;
   }
 }
